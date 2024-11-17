2d61d1c57f084a61d57c8b1c62982092ee68dc68
Mark Silliman <mwsilliman@gmail.com>
2024-11-17 02:55:09 +0000

    Remove unnecessary blank line in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,6 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
