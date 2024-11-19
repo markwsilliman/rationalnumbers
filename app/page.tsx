32991c648b0598f4eb2e46e372e6a1588cef7cca
Mark Silliman <mwsilliman@gmail.com>
2024-11-19 20:14:54 +0000

    header

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function BlogPage() {
       posts={posts}
       initialDisplayPosts={initialDisplayPosts}
       pagination={pagination}
-      title="All Posts"
+      title="Why Math Matters"
     />
   )
 }
